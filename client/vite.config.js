53e5aa4f2e8f170ed00bcd537072ba8ae35c5387
vishveshkumarnelli-netizen <vishveshkumarnelli@gmail.com>
2026-04-07 10:00:16 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://grocify-app-08jf.onrender.com',
         changeOrigin: true,
       },
     },
